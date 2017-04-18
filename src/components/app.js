d9d4cb10226e84786056a8ea3f71d2200d6010e4
MatteoBosi <matteo.bosi95@gmail.com>
2017-04-18 20:17:58 +0200

    partenza

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+'use strict';
+import React from 'react';
+
+export default class App extends React.Component{
+    constructor(){
+        super();
+        console.log('app started');    
+    }
+	render(){
+        return(
+            <section>
+			
+			</section>
+        )
+    }
+}
