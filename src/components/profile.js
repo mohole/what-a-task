f5cc02aa76f6bb30fd0a8e89ac6ea92369d754e8
tizianoborgato93 <tiziano.borgato@gmail.com>
2017-04-27 18:06:43 +0200

    Profile page
    Aggiornamento profile page

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,20 +4,29 @@ import React from 'react';
 export default class Profile extends React.Component{
     constructor(){
         super();
+        this.state={
+          nome: 'Tiziano Borgato',
+          imgUtente: 'http://lorempixel.com/200/200',
+          corso: 'Web & App',
+          mail: 'tiziano.borgato@gmail.com',
+          telefono: '334 1301904'
+        }
     }
+
+
 	render(){
         return(
             <section>
             <div className="container">
               <div className="row">
-                <div className="col-xs-12 text-center"><img src="http://lorempixel.com/200/200" />
+                <div className="col-xs-12 text-center"><img src={this.state.imgUtente} />
                 </div>
-                <div className="col-xs-12 text-center"><h1>Nome Cognome</h1>
+                <div className="col-xs-12 text-center"><h1>{this.state.nome}</h1>
                 </div>
                 <div className="col-xs-12">
-                  <p>Corso: Lorem ipsum.</p>
-                  <p>Mail: pippo@pippo</p>
-                  <p>Tel: 0123456789</p>
+                  <p>Corso: {this.state.corso}</p>
+                  <p>Mail: {this.state.mail}</p>
+                  <p>Tel: {this.state.telefono}</p>
                 </div>
                 <div className="col-xs-12">
                   <a href="btn btn-primary"><i className="glyphicon glyphicon-certificate"></i></a>
