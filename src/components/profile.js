4c15b77b31d7cef887a7b70280e97313cbaf8c3f
tizianoborgato93 <tiziano.borgato@gmail.com>
2017-04-27 22:10:44 +0200

    Profile.js
    Pagina profilo in mui e non più in bootstrap

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,27 +4,27 @@ import React from 'react';
 export default class Profile extends React.Component{
     constructor(props){
         super(props);
-      
+
     }
 
 
 	render(){
         return(
             <section>
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-12 text-center"><img src={this.props.avatar_urls} />
+            <div className="mui-container">
+              <div className="mui-row">
+                <div className="mui-col-xs-12 text-center"><img src={this.props.avatar_urls} />
                 </div>
-                <div className="col-xs-12 text-center"><h1>{this.props.first_name} {this.props.last_name}</h1>
+                <div className="mui-col-xs-12 text-center"><h1>{this.props.first_name} {this.props.last_name}</h1>
                 </div>
-                <div className="col-xs-12">
+                <div className="mui-col-xs-12">
                   <p>Mail: {this.props.email}</p>
                   <p>Descrizione: {this.props.description}</p>
                 </div>
-                <div className="col-xs-12">
-                  <a href="btn btn-primary"><i className="glyphicon glyphicon-certificate"></i></a>
-                  <a href="btn btn-primary"><i className="glyphicon glyphicon-certificate"></i></a>
-                  <a href="btn btn-primary"><i className="glyphicon glyphicon-certificate"></i></a>
+                <div className="mui-col-xs-12">
+                  <a className="mui-btn mui-btn--fab mui-btn--primary">+</a>
+                  <a className="mui-btn mui-btn--fab mui-btn--primary">+</a>
+                  <a className="mui-btn mui-btn--fab mui-btn--primary">+</a>
                 </div>
               </div>
             </div>
