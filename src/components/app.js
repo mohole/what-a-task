533e44c1e116fb90eb8c0dce69a06a083965c9f0
tizianoborgato93 <tiziano.borgato@gmail.com>
2017-04-27 20:28:38 +0200

    Profile.js
    Aggiornamento pagina profile.jj

--- src/components/app.js
+++ src/components/app.js
@@ -10,7 +10,14 @@ export default class App extends React.Component{
 	render(){
         return(
             <section>
-			<Profile />
+			<Profile
+      first_name= 'Tiziano'
+      last_name= 'Borgato'
+      avatar_urls= 'http://lorempixel.com/200/200'
+      email= 'tiziano.borgato@gmail.com'
+      description= 'Sono uno studente del secondo anno di Web & Apps. Per maggiori info contattatemi al 334 1301904'
+
+      />
 			</section>
         )
     }
--- src/components/profile.js
+++ src/components/profile.js
@@ -2,15 +2,9 @@
 import React from 'react';
 
 export default class Profile extends React.Component{
-    constructor(){
-        super();
-        this.state={
-          nome: 'Tiziano Borgato',
-          imgUtente: 'http://lorempixel.com/200/200',
-          corso: 'Web & App',
-          mail: 'tiziano.borgato@gmail.com',
-          telefono: '334 1301904'
-        }
+    constructor(props){
+        super(props);
+      
     }
 
 
@@ -19,14 +13,13 @@ export default class Profile extends React.Component{
             <section>
             <div className="container">
               <div className="row">
-                <div className="col-xs-12 text-center"><img src={this.state.imgUtente} />
+                <div className="col-xs-12 text-center"><img src={this.props.avatar_urls} />
                 </div>
-                <div className="col-xs-12 text-center"><h1>{this.state.nome}</h1>
+                <div className="col-xs-12 text-center"><h1>{this.props.first_name} {this.props.last_name}</h1>
                 </div>
                 <div className="col-xs-12">
-                  <p>Corso: {this.state.corso}</p>
-                  <p>Mail: {this.state.mail}</p>
-                  <p>Tel: {this.state.telefono}</p>
+                  <p>Mail: {this.props.email}</p>
+                  <p>Descrizione: {this.props.description}</p>
                 </div>
                 <div className="col-xs-12">
                   <a href="btn btn-primary"><i className="glyphicon glyphicon-certificate"></i></a>
