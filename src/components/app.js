201891253005f02ec582f5da870a4dd74ffef4e8
MatteoBosi <matteo.bosi95@gmail.com>
2017-04-29 14:36:20 +0200

    add spinner components

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,14 @@
 'use strict';
 import React from 'react';
-
+import Spinner from './common/spinner';
 export default class App extends React.Component{
     constructor(){
         super();
-        console.log('app started');    
+        console.log('app started');
     }
 	render(){
-        return(
-            <section>
-			
-			</section>
-        )
+		return(
+			<Spinner/>
+		)
     }
 }
