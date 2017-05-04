b4781db444d6fd6f4eeb0e62ae9d99b285507592
tizianoborgato93 <tiziano.borgato@gmail.com>
2017-05-04 20:30:37 +0200

    profile e modify_profile
    Ho modificato il componente profile.js e messo il controllo dei campi
    input in modify_profile.js

--- src/components/app.js
+++ src/components/app.js
@@ -19,13 +19,6 @@ export default class App extends React.Component{
       description= 'Sono uno studente del secondo anno di Web & Apps. Per maggiori info contattatemi al 334 1301904'
       />
 
-      <ModifyProfile
-      first_name= 'Tiziano'
-      last_name= 'Borgato'
-      avatar_urls= 'http://lorempixel.com/200/200'
-      email= 'tiziano.borgato@gmail.com'
-      description= 'Sono uno studente del secondo anno di Web & Apps. Per maggiori info contattatemi al 334 1301904'
-      />
 
 			</section>
         )
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,14 +1,43 @@
 'use strict';
 import React from 'react';
+import ModifyProfile from './modify_profile';
 
 export default class Profile extends React.Component{
     constructor(props){
         super(props);
 
+        this.state={
+          isEditable: true,
+          editActive: false
+        }
+    }
+
+    editProfile(){
+      this.setState({
+        editActive: !this.state.editActive
+      })
+    }
+
+	render(){
+
+      let btnEdit='';
+    if(this.state.isEditable){
+       btnEdit = <button onClick={this.editProfile.bind(this)} className="mui-btn mui-btn--fab mui-btn--primary">+</button> ;
     }
 
 
-	render(){
+    if(this.state.editActive && this.state.isEditable){
+        return(
+      <ModifyProfile
+          first_name= {this.props.first_name}
+          last_name= {this.props.last_name}
+          avatar_urls= {this.props.avatar_urls}
+          email= {this.props.email}
+          description= {this.props.description}
+          undo={this.editProfile.bind(this)}
+          />
+            )
+        }else{
         return(
             <section>
             <div className="mui-container">
@@ -24,11 +53,12 @@ export default class Profile extends React.Component{
                   <p>Descrizione: {this.props.description}</p>
                 </div>
                 <div className="mui-col-xs-12">
-                  <a className="mui-btn mui-btn--fab mui-btn--primary">+</a>
+                  {btnEdit}
                 </div>
               </div>
             </div>
-			</section>
-        )
+			      </section>
+            )
+          }
     }
 }
