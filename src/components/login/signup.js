5fa31c3e6e535d8d44210a3560624553e9281bc7
Becchina <rebiguzzo@gmail.com>
2017-05-16 21:07:21 +0200

    signup_state
    input con onChange

--- src/components/login/signup.js
+++ src/components/login/signup.js
@@ -1,14 +1,19 @@
 'use strict';
 import React from 'react';
+import Spinner from './../common/spinner';
 import {Backend} from './../../backend';
 export default class Signup extends React.Component{
     constructor(props){
         super(props);
         
         this.state = {
-                utente: '',
-                password: '',
-                ClassNameControl:'mui-textfield'
+                newUser:{},
+                utente:'',
+                email:'',
+                password:'',
+                prs:'',
+                image:{},
+                privacyCheck:false
         };
     }
     
@@ -19,7 +24,36 @@ export default class Signup extends React.Component{
             [elemento]: input.value  
         }); 
       }
-
+	uploadFile(evt){
+		console.log(evt.target.files);
+		const file=evt.target.files[0];
+		console.log(file.size);
+		var formData = new FormData();
+      	formData.append('image', file, file.name);
+		this.setState({
+            image : formData
+        });
+		console.log(formData);
+	}
+    checkPrivacy(evt){
+		console.log(!this.state.privacyCheck);
+        this.setState({
+            privacyCheck : !this.state.privacyCheck
+        });
+    }    
+    resetForm(){
+		console.log('reset');
+		console.log(this.state);
+		this.setState({
+                newUser:{},
+                utente:'',
+                email:'',
+                password:'',
+                prs:'',
+                image:{},
+                privacyCheck:false
+		});
+	}
 	render(){
         
         return(
@@ -28,58 +62,66 @@ export default class Signup extends React.Component{
                     <div className="mui-row">
                     <div className="mui-col-xs-12">
                         <form action="#" className="mui-form" encType="multipart/form-data">
-                                <legend>Sign up</legend>
-
-                                <div className="mui-textfield">
-                                    <input type="file" name="imgProfilo"/>  
-                                    <label>Immagini</label>
-                                </div>
-                                
+                                <h3>Sign up</h3>
+            
+                                <div className="mui-textfield">   
+                                    <input type="file" placeholder="Immagine profilo" 
+                                    onChange={this.uploadFile.bind(this)} />
+                                </div> 
+        
                                 <div className="mui-textfield mui-textfield--float-label">
-                                    <input name="utente" type="text" required={true} />
+                                    <input name="utente" type="text" onChange={this.writing.bind(this)} value={this.state.utente}/>
                                     <label>Nome utente</label>
-                                </div>
+                                </div>                                           
 
                                 <div className="mui-textfield mui-textfield--float-label">
-                                    <textarea></textarea>
+                                    <textarea name="prs" type="text" onChange={this.writing.bind(this)}
+                                    value={this.state.prs}></textarea>
                                     <label>Presentazione</label>
                                 </div>
 
                                 <div className="mui-select">
-                                    <select name="ruolo">
+                                    <select>
                                         <option value="prof">Professore</option>
                                         <option value="staff">Staff</option>
                                         <option value="studente">Studente</option>  
                                     </select>
                                     <label>Ruolo</label>
                                 </div>
-
+                                        
                                 <div className="mui-select">
-                                    <select name="corso">
-                                        <option value="animation" >3D Animation</option>
-                                        <option value="acting">Acting</option>         
-                                        <option value="film">Filmaking</option>
+                                    <select defaultValue="web">
+                                        <option value="animation">3D Animation</option>
+                                         <option value="acting">Acting</option>
+                                        <option value="film">Filmaking</option>   
                                         <option value="foto">Fotografia</option>
-                                        <option value="fumetto">Fumetto</option>
-                                        <option value="grafica">Grafica</option>
-                                        <option value="story">Storytelling</option>
+                                        <option value="fumetto">Fumetto</option>  
+                                        <option value="grafica">Grafica</option> 
+                                        <option value="story">Storytelling</option>                   
                                         <option value="web">Web&App</option>
                                     </select>
-                                    <label>Corso seguito</label>
+                                    <label>Ruolo</label>
                                 </div>
 
                                 <div className="mui-textfield mui-textfield--float-label">
-                                    <input type="email" required={true} />
+                                    <input type="email" value={this.state.email} onChange={this.writing.bind(this)} />
                                     <label>Email</label> 
                                 </div>
 
                                 <div className="mui-textfield mui-textfield--float-label">
-                                    <input name="password" type="password" required={true} />
-                                    <label>Password</label> 
+                                    <input name="password" type="password" onChange={this.writing.bind(this)} />
+                                    <label>Password</label>
                                 </div>
-            
+    
+                                <div className='mui-checkbox'>
+                                    <label>Termini e condizioni</label><br/>
+                                    <input type="checkbox" checked={this.state.privacyCheck} onChange={this.checkPrivacy.bind(this)}/><span> Accetto i <a href="#">termini della privacy</a></span>
+                                </div>
+                                
                                 <div className="mui-form-group">
-                                    <button type="submit" className="mui-btn mui-btn--danger">Registrati</button>
+                                    <button type="submit" className="mui-btn ">Registrati</button>
+                                    <button type="button" onClick={this.resetForm.bind(this)} 
+                                    className="mui-btn mui-btn--danger">Annulla</button>
                                 </div>
                         </form>
                     </div>
