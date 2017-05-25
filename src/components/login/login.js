1f1ad67c80b3d89229fffd4b761a63fde80e5fae
pignax96 <giovanni.pignataro@hotmail.com>
2017-05-25 18:25:07 +0200

    update login style
    new login style

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -54,35 +54,33 @@ export default class Login extends React.Component{
 	render(){
 
         return(
+            
+        
             <section>
-
-               <div className="mui-container">
-                   <div className="mui-row">
-
-                   <form className="mui-form">
-
-                   <legend>Entra in What A Task</legend>
-
-                   <div className={this.state.ClassNameControl}>
-
-                   <div className="mui-textfield mui-textfield--float-label">
-                   <input name="utente" type="text" onChange={this.writing.bind(this)} value={this.state.utente} />
-                   <label>Nome Utente</label>
-                   </div>
-
-                   <div className="mui-textfield mui-textfield--float-label">
-                   <input name="password" type="password" onChange={this.writing.bind(this)} />
-                   <label>Password</label>
-                   </div>
-
-                   </div>
-
-                   <button type="button" className="mui-btn mui-btn--raised" onClick={this.checkLogin.bind(this)}>Entra</button>
-
-                   </form>
-
-                </div>
+            
+      <div className="background">
+        <div className="vertical-center">
+          <div className="mui-container">
+            <div className="mui-row">
+              <div className="mui-col-xs-12">
+                <form className="mui-form">
+                  <legend className="logo">WHAT A TASK</legend>
+                  <div className="mui-textfield mui-textfield--float-label">
+                    <input name="utente" className="my-input" type="text" onChange={this.writing.bind(this)} value={this.state.utente} />
+                    <label className="my-label">Utente</label>
+                  </div>
+                  <div className={this.state.ClassNameControl}>
+                    <input name="password" className="my-input" type="password" onChange={this.writing.bind(this)} />
+                    <label className="my-label">Password</label>
+                  </div>
+                  <button type="submit" className="my-button mui-col-xs-12 mui-btn mui-btn--raised" onClick={this.checkLogin.bind(this)}>LOGIN</button>
+                  <button type="submit" className="my-button mui-col-xs-12 mui-btn mui-btn--raised">REGISTRATI</button>
+                </form>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
 
 			</section>
         )
